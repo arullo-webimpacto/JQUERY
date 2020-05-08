d7a9305107cdad3a193438df1be3fbf9da132116
arullo <arullo@webimpacto.es>
2020-05-08 11:39:20 +0200

    20.Animaciones2

---
+++ Animaciones_2/js/animaciones.js
@@ -0,0 +1,25 @@
+(function(){
+
+	var $cajaRoja = $(".cajaRoja");
+
+	$("#botTamano").on("click",function () {
+		$cajaRoja.animate({
+
+			width: "+=100px",
+			height: "+=100px",
+
+		},function () {
+			console.log("Termino la animacion del tamaño");
+			
+		}).animate({
+			width: "-=100px",
+			height: "-=100px",
+		}).animate({
+			opacity: 0.1,
+		},1500,function () {
+			$(this).remove();
+		});
+	});
+
+
+})();
