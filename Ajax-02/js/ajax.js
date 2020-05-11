0ab22264e77caeb786c020e5092198f345c5109f
arullo <arullo@webimpacto.es>
2020-05-11 14:41:00 +0200

    33.Leer Arrays en JSON

---
+++ Ajax-02/js/ajax.js
@@ -0,0 +1,48 @@
+(function(){
+
+
+	$.ajax({
+		type: 'GET',
+		url : 'http://www.json-generator.com/api/json/get/bZQMPEdcSW?indent=2',
+		dataType: 'json'
+	})
+	.done(function( data ){
+		
+		var personas = data;
+
+		for (var i = 0; i < personas.length; i++) {
+
+			var persona = personas[i];
+			var tags = "";
+			for (var j = 0; j < persona.tags.length; j++) {
+				tags += '<span class="label label-danger">'+persona.tags[j]+ "</span> ";
+				
+			}
+
+			var html = "";
+			html += '<tr>';
+			html += '	<th>'+persona.name+'</th>';
+			html += '	<th>'+persona.age+'</th>';
+			html += '	<th>'+tags+'</th>';
+			html += '</tr>';
+
+			$(".table").append(html);
+			
+		}
+
+	})
+	.fail(function(){
+		console.log("Fallo!");
+	})
+	.always(function(){
+		console.log("Completo!");
+	});
+
+
+
+
+
+
+
+
+})();
