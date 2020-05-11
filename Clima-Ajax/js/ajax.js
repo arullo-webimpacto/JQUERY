d0c1194f36664985068dcf2c73976e727f30f030
arullo <arullo@webimpacto.es>
2020-05-11 15:03:47 +0200

    34.Tarea Aplicacion del clima y solucion

--- Clima-Ajax/js/ajax.js
+++ Clima-Ajax/js/ajax.js
@@ -1,13 +1,13 @@
 (function(){
 
-	var Latitude = 9.975941;
-	var Longitude = -84.007505;
+	var Latitude = 39.869358;
+	var Longitude = -4.022209;
 
 
 	$.ajax({
 		type: 'GET',
 		url : 'http://api.openweathermap.org/data/2.5/weather?lat='+ Latitude +'&lon=' + Longitude + "&units=metric&appid=9f50a805aa0089a1edd1829a5db029f0",
-		dataType: 'jsonp'
+		dataType: 'json'
 	})
 	.done(function( data ){
 		
@@ -15,7 +15,7 @@
 
 		console.log( data ); // Se imprime en consola la api
 
-
+		mostrar_data( data );
 	})
 	.fail(function(){
 		console.log("Fallo!");
@@ -24,6 +24,22 @@
 		console.log("Completo!");
 	});
 
+	function mostrar_data( data ){
+
+		// Imagen
+		var url = "img/" + data.weather[0].icon + ".png";
+		$(".imgClima").attr('src', url);
+
+		var temperatura = Math.round(data.main.temp);
+		$(".lblTemperatura").html( temperatura + "&#176;" );
+
+
+		$(".divLoading").fadeOut(200,function(){
+			$(".divInfo").fadeIn(200);
+		});
+					
+
+	}
 
 
 
